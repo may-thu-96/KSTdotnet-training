8510e8a84124d4e2941a09f88e4962f8e7704b75
KhinSableThu <khinsablethu.96@gmail.com>
2025-01-07 11:00:35 +0630

    Fix: RestAPI

---
+++ KSTdotnet-training.RestApi/Controllers/BlogsController.cs
@@ -0,0 +1,111 @@
+﻿//using KSTdotnet_training.ConsoleApp.Models;
+using KSTdotnet_training.DataBase.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KSTdotnet_training.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        private readonly AppDbContext _db = new AppDbContext();
+
+        [HttpGet]
+        public IActionResult GetBlog()
+        {
+            var lst = _db.TblBlogs.AsNoTracking().ToList();
+
+            return Ok(lst);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlog(int id)
+        {
+            var item = _db.TblBlogs.AsNoTracking().FirstOrDefault(x=> x.BlogId==id);
+
+            if(item is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(TblBlog blog)
+        {
+            _db.Add(blog);
+
+            _db.SaveChanges();
+
+            return Ok(blog);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlog(int id, TblBlog blog)
+        {
+            var item = _db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent; 
+
+            _db.Entry(item).State=EntityState.Modified;
+            _db.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPatch]
+        public IActionResult PatchBlog(int id, TblBlog blog)
+        {
+            var item = _db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+            if(!string.IsNullOrEmpty(blog.BlogTitle))
+            {
+                item.BlogTitle = blog.BlogTitle;
+            }
+
+            if (!string.IsNullOrEmpty(blog.BlogAuthor))
+            {
+                item.BlogAuthor = blog.BlogAuthor;
+            }
+
+            if (!string.IsNullOrEmpty(blog.BlogContent))
+            {
+                item.BlogContent = blog.BlogContent;
+            }
+
+
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteBlog(int id)
+        {
+            var item = _db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+            item.DeleteFlag = true;
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+            return Ok();
+        }
+    }
+}
