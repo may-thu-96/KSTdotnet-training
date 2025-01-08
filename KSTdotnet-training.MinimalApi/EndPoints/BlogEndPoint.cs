a93a6e32bcd5b61173fc10d1403f7f6531eca7b8
KhinSableThu <khinsablethu.96@gmail.com>
2025-01-08 15:19:40 +0630

    fix: Minimal Api

---
+++ KSTdotnet-training.MinimalApi/EndPoints/BlogEndPoint.cs
@@ -0,0 +1,73 @@
+﻿
+
+namespace KSTdotnet_training.MinimalApi.EndPoints
+{
+    public static class BlogEndPoint
+    {
+        public static void UseBlogEndPoint(this IEndpointRouteBuilder app)
+        {
+
+            app.MapGet("/blogs", () =>
+            {
+                AppDbContext db = new AppDbContext();
+                var lst = db.TblBlogs.AsNoTracking().ToList();
+                return Results.Ok(lst);
+            }).WithName("GetBlog")
+            .WithOpenApi();
+
+            app.MapGet("/blogs/{id}", (int id) =>
+            {
+                AppDbContext db = new AppDbContext();
+                var item = db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+                if (item is null)
+                {
+                    return Results.BadRequest("No Data Found.");
+                }
+                return Results.Ok(item);
+            }).WithName("GetByIDBlog")
+            .WithOpenApi();
+
+            app.MapPost("/blogs", (TblBlog blog) =>
+            {
+                AppDbContext db = new AppDbContext();
+                db.TblBlogs.Add(blog);
+                db.SaveChanges();
+                return Results.Ok(blog);
+            }).WithName("CreateBlog")
+            .WithOpenApi();
+
+            app.MapPut("/blogs/{id}", (int id, TblBlog blog) =>
+            {
+                AppDbContext db = new AppDbContext();
+                var item = db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+                if (item is null)
+                {
+                    return Results.BadRequest("No Data Found.");
+                }
+                item.BlogTitle = blog.BlogTitle;
+                item.BlogAuthor = blog.BlogAuthor;
+                item.BlogContent = blog.BlogContent;
+
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return Results.Ok();
+            }).WithName("UpdateBlog")
+            .WithOpenApi();
+
+            app.MapDelete("/blogs/{id}", (int id) =>
+            {
+                AppDbContext db = new AppDbContext();
+                var item = db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+                if (item is null)
+                {
+                    return Results.BadRequest("No Data Found.");
+                }
+                db.Entry(item).State = EntityState.Deleted;
+                db.SaveChanges();
+                return Results.Ok();
+            }).WithName("DeleteBlog")
+            .WithOpenApi();
+
+        }
+    }
+}
---
+++ KSTdotnet-training.MinimalApi/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using KSTdotnet_training.DataBase.Models;
+global using Microsoft.EntityFrameworkCore; 
+global using KSTdotnet_training.MinimalApi.EndPoints;
+global using Microsoft.AspNetCore.Builder; 
